9cf22861f22eb0772fa80e5f8c79004def143dce
Daniel Vera <dvera@devsu.com>
2019-12-09 14:18:35 -0500

    fix run

--- api.py
+++ api.py
@@ -32,5 +32,4 @@ api.add_resource(Microservice2,
 if __name__ == '__main__':
     if environ.get('SERVER_NAME') is not None:
         app.config['SERVER_NAME'] = environ.get('SERVER_NAME')
-    app.config['SERVER_NAME'] = '0.0.0.0:5000'
-    app.run(debug=True)
+    app.run(host='0.0.0.0', port=8080, debug=True)
